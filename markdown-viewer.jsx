630f748b358f93e12b3d592847fea97aa8f42b7b
Matt Rothenberg <mattrothenberg@users.noreply.github.com>
2021-11-08 12:55:54 -0500

    Create markdown-viewer.jsx

---
+++ markdown-viewer.jsx
@@ -0,0 +1,13 @@
+import ReactMarkdown from 'react-markdown' 
+
+export default function MarkdownViewer (props) {
+  const { repo, owner, content } = props;
+  console.log(props);
+
+  return (
+    <ReactMarkdown>
+      # Hello World
+      {repo} x {owner}
+    </ReactMarkdown>
+  );
+}

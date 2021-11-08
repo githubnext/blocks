e99a53148573b53828a5b9168d1c480c65fb9166
Matt Rothenberg <mattrothenberg@users.noreply.github.com>
2021-11-08 13:20:36 -0500

    Update markdown-viewer.jsx

--- markdown-viewer.jsx
+++ markdown-viewer.jsx
@@ -3,8 +3,9 @@ import ReactMarkdown from 'react-markdown'
 function Viewer (props) {
   const { content, meta } = props;
   const { owner, repo } = meta;
+  console.log('userland', props, meta);
   
-  const markdown = `# Hello World ${owner}x ${repo}`;
+  const markdown = `# Hello World`;
 
   return (
     <ReactMarkdown children={markdown}></ReactMarkdown>

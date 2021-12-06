525ef7a4d589798d7bc55f2d37fb7b08064ff229
Irene Alvarado <irealva@users.noreply.github.com>
2021-12-06 12:46:16 -0500

    Update processing-sketch.js

--- processing-sketch.js
+++ processing-sketch.js
@@ -1,8 +1,18 @@
-function setup() {
-  createCanvas(400, 400);
-}
+// Have to create the sketch in instance mode
+// learn more here: https://github.com/processing/p5.js/wiki/Global-and-instance-mode
 
-function draw() {
-  background(220);
-  ellipse(50,50,80,80);
-}
+const s = ( sketch ) => {
+
+  let x = 100;
+  let y = 100;
+
+  sketch.setup = () => {
+    sketch.createCanvas(200, 200);
+  };
+
+  sketch.draw = () => {
+    sketch.background(0);
+    sketch.fill(255);
+    sketch.rect(x,y,50,50);
+  };
+};

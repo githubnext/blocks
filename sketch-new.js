030098efb4d9e8a31d99487c76f0b1c4c5a068dc
Irene Alvarado <irealva@users.noreply.github.com>
2022-03-09 10:39:12 -0800

    Create sketch-new.js

---
+++ sketch-new.js
@@ -0,0 +1,17 @@
+let x = 100;
+let y = 100;
+
+function setup() {
+  createCanvas(400, 400);
+  background(0);
+}
+
+function draw() {
+  if (mouseIsPressed) {
+      fill(0);
+    } else {
+      fill(255);
+    }
+
+    ellipse(mouseX, mouseY, 80, 80);
+}

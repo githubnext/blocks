621a8e09d4c7e430f9e7d1473c63b46162059588
Jake Donham <jaked@github.com>
2022-03-15 11:01:34 -0700

    processing block supports global-mode sketches

--- processing-sketch.js
+++ processing-sketch.js
@@ -1,23 +1,14 @@
-// Have to create the sketch in instance mode
-// learn more here: https://github.com/processing/p5.js/wiki/Global-and-instance-mode
+function setup() {
+  createCanvas(400, 400);
+  background(0);
+}
 
-const s = (sketch) => {
-  let x = 100;
-  let y = 100;
-
-  sketch.setup = () => {
-    sketch.createCanvas(400, 400);
-    sketch.background(0);
+function draw() {
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
   }
 
-  sketch.draw = () => {
-    if (sketch.mouseIsPressed) {
-      sketch.fill(0);
-    } else {
-      sketch.fill(255);
-    }
-
-    sketch.ellipse(sketch.mouseX, sketch.mouseY, 80, 80);
-  }
-};
-const P5 = new p5(s);
+  ellipse(mouseX, mouseY, 80, 80);
+}

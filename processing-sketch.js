9ef4e35546f93db2b4530a7f4bae8c2005ffd81b
Amelia Wattenberger <wattenberger@github.com>
2022-07-14 15:52:24 -0700

    feat: updated processing-sketch.js programatically

--- processing-sketch.js
+++ processing-sketch.js
@@ -1,14 +1,9 @@
 function setup() {
   createCanvas(400, 400);
-  background(0);
+  background(255);
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255, 192, 203);
-  }
-
+  fill(mouseX, mouseY, mouseX + mouseY);
   ellipse(mouseX, mouseY, 80, 80);
 }
